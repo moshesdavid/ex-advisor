81c961bd3cd9452c971c8e8897d440eaae3d0cff
moshesdavid <davidmbq@gmail.com>
2025-06-30 19:12:55 +0000

    autosave 2025-06-30 19:12:55

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     return render(request, "reviews/home.html")
 
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, "reviews/registration_success.html")
+    else:
+        form = UserCreationForm()
+    return render(request, "reviews/signup.html", {"form": form})
