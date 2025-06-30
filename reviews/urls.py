da2893a9e9a2b55b659e4edcf1f1c6d5b622c734
moshesdavid <davidmbq@gmail.com>
2025-06-30 18:52:51 +0000

    autosave 2025-06-30 18:52:51

---
+++ reviews/urls.py
@@ -0,0 +1,8 @@
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        
