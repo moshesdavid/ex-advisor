2f769149a47af73a0b058d677232e7abde60f0ed
moshesdavid <davidmbq@gmail.com>
2025-06-30 19:02:53 +0000

    autosave 2025-06-30 19:02:53

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,4 +5,8 @@ from django.contrib.auth.forms import UserCreationForm
 def signup(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+
 
