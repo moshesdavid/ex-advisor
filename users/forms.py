f55876dc9e19ac13a39b9f81ec5310d4dc54398a
moshesdavid <davidmbq@gmail.com>
2025-07-18 15:48:24 +0000

    autosave 2025-07-18 15:48:24

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
-def register_user(username, password, email):
-    if User.objects.filter(username=)
+
+print(User.objects.all())
