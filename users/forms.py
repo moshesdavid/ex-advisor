e37c8502b858bdaa005ffceb90b26e2c59a2954f
moshesdavid <davidmbq@gmail.com>
2025-07-19 19:48:03 +0000

    autosave 2025-07-19 19:48:03

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,2 @@
-from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
-
-def users_list():
-    usuarios = User.objects.all()
-    print(usuarios)
 
 
---
+++ users/services.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+def users_list():
+    usuarios = User.objects.all()
+    print(usuarios)
+
+
+def create_user(username=str, password=str):
+    # Comrpueba si existe el usuario
+
+    # Crea el usuario+
+    User.objects.create_user(username=username, password=password)
+
+    # Avisamos de que se ha creado correctamente
+    print(f"Usuario {}")
