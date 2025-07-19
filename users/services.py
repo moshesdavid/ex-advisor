045abbaa947c2dcdc2c63c6ce31c7e12c3ef19e1
moshesdavid <davidmbq@gmail.com>
2025-07-19 19:58:06 +0000

    autosave 2025-07-19 19:58:06

--- users/services.py
+++ users/services.py
@@ -3,17 +3,35 @@ from django.core.exceptions import ValidationError
 
 def users_list():
     usuarios = User.objects.all()
-    print(usuarios)
+    return usuarios
+
+def check_user_exist(username):
+    """
+    Función que revisa si existe un usuario y devuelve True/False
+    """    
+    # Recuperamos el nombre de los usuarios
+    usuarios = users_list()
+
+    # Revisamos si el usuario existe en la lista
+    if username in usuarios:
+        return True
+    else:
+        return False
 
 
 def create_user(username=str, password=str):
     # Comrpueba si existe el usuario
+    user_exist = check_user_exist(username)
 
-    # Crea el usuario+
-    User.objects.create_user(username=username, password=password)
+    if user_exist:
+        # Crea el usuario
+        User.objects.create_user(username=username, password=password)
 
-    # Avisamos de que se ha creado correctamente
-    print(f"Usuario {username} creado correctamente")
+        # Avisamos de que se ha creado correctamente
+        print(f"Usuario {username} creado correctamente")
+
+    else:
+        print(f"El usuario {username} ya existe. Prueba con otro nombre.")
 
 
 
