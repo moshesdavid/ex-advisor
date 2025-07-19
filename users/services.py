19286e5e26b4e78bbd789f3810f0677dd4e36257
moshesdavid <davidmbq@gmail.com>
2025-07-19 20:03:08 +0000

    autosave 2025-07-19 20:03:08

--- users/services.py
+++ users/services.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
 def users_list():
-    usuarios = User.objects.all()
+    usuarios = list(User.objects.all())
     return usuarios
 
 def check_user_exist(username):
