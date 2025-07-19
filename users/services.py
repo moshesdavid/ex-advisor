83bdb61b5a4ee1e23e75f8fa8205326eb8b2d228
moshesdavid <davidmbq@gmail.com>
2025-07-19 22:08:21 +0000

    Creadas funciones: create_user y user_info

--- users/services.py
+++ users/services.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
-def users_list():
-    usuarios = list(User.objects.all())
-    return usuarios
+def users_list(show: bool = False) -> list[str]:
+    lista_usuarios = [user.username for user in User.objects.all()]     
+    if show==True:
+        print(f"Lista usuarios actual: {lista_usuarios}")
+    return lista_usuarios
 
 def check_user_exist(username):
     """
@@ -18,20 +20,30 @@ def check_user_exist(username):
     else:
         return False
 
-
-def create_user(username=str, password=str):
+def create_user(username=str, password=str, email=str):
     # Comrpueba si existe el usuario
     user_exist = check_user_exist(username)
 
     if user_exist:
         # Crea el usuario
-        User.objects.create_user(username=username, password=password)
+        User.objects.create_user(username=username, password=password, email=email)
 
         # Avisamos de que se ha creado correctamente
         print(f"Usuario {username} creado correctamente")
 
     else:
         print(f"El usuario {username} ya existe. Prueba con otro nombre.")
+
+def user_info(username:str, show:bool = False):
+    """
+    Devuelve la información de un usuario concreto
+    """
+    user = User.objects.filter(username=username).first()
+    if show:
+        print(f"Información del usuario: ")
+        print(f"Usuario: {user.username}")
+        print(f"Password: {user.password}")    
+    return user
 
 
 
