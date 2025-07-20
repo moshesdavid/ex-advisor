3aa86a6762a6f2d62cf9545a52bebfdf409ba9a1
moshesdavid <davidmbq@gmail.com>
2025-07-20 11:32:50 +0000

    autosave 2025-07-20 11:32:49

--- exadvisor/urls.py
+++ exadvisor/urls.py
@@ -6,6 +6,6 @@ from reviews.views import home
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home, name="home"),
-    path("login/", auth_views.LoginView.as_view(template_name="reviews/login.html"), name="login"),
+    path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
 ]
