44b326db48568dae840a3261cca760c0b71eaecb
moshesdavid <davidmbq@gmail.com>
2025-07-20 11:57:53 +0000

    autosave 2025-07-20 11:57:53

--- exadvisor/settings.py
+++ exadvisor/settings.py
@@ -59,7 +59,7 @@ LOGIN_REDIRECT_URL = '/'
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [BASE_DIR / "exadvisor" / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
