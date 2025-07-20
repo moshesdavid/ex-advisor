911e231db4ab1999ede0d43c118825afa01ae7f6
moshesdavid <davidmbq@gmail.com>
2025-07-20 12:38:03 +0000

    autosave 2025-07-20 12:38:03

--- reviews/views.py
+++ reviews/views.py
@@ -1,11 +1,12 @@
 # reviews/views.py
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 @login_required
 def home(request):
     return render(request, "home.html", {"usuario": request.user})
 
-@login_required
-def home(request):
-    return render(request, "home.html", {"usuario": request.user})
+def logout_view(request):
+    logout(request)
+    return redirect("login")
